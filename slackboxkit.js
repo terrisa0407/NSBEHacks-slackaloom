31f8b0cf3b9beca22343ed19856e77bc038a69b9
Weinihsiang <117528516+Weinihsiang@users.noreply.github.com>
2023-03-05 11:09:03 -0500

    Create slackboxkit.js

---
+++ slackboxkit.js
@@ -0,0 +1,168 @@
+
+{
+	"blocks": [
+		{
+			"type": "divider"
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Choose a picture for your life!!!\n\n",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "By managing our finances wisely, we can secure a stable future and achieve our financial goals."
+			},
+			"accessory": {
+				"type": "image",
+				"image_url": "http://floorfx.ca/wp-content/uploads/2018/06/5_0016_apple-brainstorming-business-908288.jpg",
+				"alt_text": "cute cat"
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "😀",
+						"emoji": true
+					},
+					"value": "click_me_123",
+					"url": "http://127.0.0.1:8000/slackaloom/finance"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "🥱",
+						"emoji": true
+					},
+					"value": "click_me_123",
+					"url": "https://www.slalom.com/case-studies"
+				}
+			]
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Let's work together to create a society where everyone feels valued and included."
+			},
+			"accessory": {
+				"type": "image",
+				"image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+				"alt_text": "cute cat"
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "😀",
+						"emoji": true
+					},
+					"value": "click_me_123",
+					"url": "http://127.0.0.1:8000/slackaloom/home"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "🥱",
+						"emoji": true
+					},
+					"value": "click_me_123",
+					"url": "https://www.slalom.com/case-studies"
+				}
+			]
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Prioritizing our health is the best investment we can make for a fulfilling and vibrant life."
+			},
+			"accessory": {
+				"type": "image",
+				"image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjhhYQthZ7Qju6c_oFzvh_DWZHL_sy5ZDr2g&usqp=CAU",
+				"alt_text": "cute cat"
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "😀",
+						"emoji": true
+					},
+					"value": "click_me_123",
+					"url": "http://127.0.0.1:8000/slackaloom/healthfirst"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "🥱",
+						"emoji": true
+					},
+					"value": "click_me_123",
+					"url": "https://www.slalom.com/case-studies"
+				}
+			]
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Education is the key that unlocks the door to a world of endless opportunities and possibilities."
+			},
+			"accessory": {
+				"type": "image",
+				"image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCp1XSAbZiLT0ALBFo70RyYPAHhhI4cPp9Qg&usqp=CAU",
+				"alt_text": "cute cat"
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "😀",
+						"emoji": true
+					},
+					"value": "click_me_123",
+					"url": "http://127.0.0.1:8000/slackaloom/education"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "🥱",
+						"emoji": true
+					},
+					"value": "click_me_123",
+					"url": "https://www.slalom.com/case-studies"
+				}
+			]
+		}
+	]
+}
+
